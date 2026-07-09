685000a51293d74d79e522f36a398056973d8017
Mohammed Mohammed Ibrahim <72116613+mohammedimk@users.noreply.github.com>
2026-07-09 19:51:29 +0100

    Update categories via migration

---
+++ store/migrations/0003_update_categories.py
@@ -0,0 +1,57 @@
+from django.db import migrations
+from django.utils.text import slugify
+
+
+def update_categories(apps, schema_editor):
+    """Automatically update categories on deploy"""
+    Category = apps.get_model('store', 'Category')
+    Product = apps.get_model('store', 'Product')
+    
+    # Create new categories
+    new_categories = [
+        ('Captans', 'captans'),
+        ('Caps', 'caps'),
+        ('Jeans/Shirt', 'jeansshirt'),
+        ('Blouse', 'blouse'),
+        ('Shoes', 'shoes'),
+        ('Skirt', 'skirt'),
+        ('Scarfs', 'scarfs'),
+        ('Hijabs', 'hijabs'),
+        ('Textiles', 'textiles'),
+        ('Shedda', 'shedda'),
+        ('Gatzner', 'gatzner'),
+    ]
+    
+    for name, slug in new_categories:
+        Category.objects.get_or_create(slug=slug, defaults={'name': name})
+    
+    # Move products from old categories to Captans
+    try:
+        captans = Category.objects.get(slug='captans')
+        old_slugs = ['dresses', 'ankara', 'coord', 'outerwear', 'accessories']
+        
+        for old_slug in old_slugs:
+            try:
+                old_cat = Category.objects.get(slug=old_slug)
+                Product.objects.filter(category=old_cat).update(category=captans)
+                old_cat.delete()
+            except Category.DoesNotExist:
+                pass
+    except Category.DoesNotExist:
+        pass
+
+
+def reverse(apps, schema_editor):
+    """Reverse is optional - we'll just pass"""
+    pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('store', '0002_supabase_upload_paths'),  # or whatever your last migration was
+    ]
+
+    operations = [
+        migrations.RunPython(update_categories, reverse),
+    ]
