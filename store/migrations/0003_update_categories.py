ffb01628ca28b25c45e8fd4f4972d30abc7858d3
Mohammed Mohammed Ibrahim <72116613+mohammedimk@users.noreply.github.com>
2026-07-11 08:03:49 +0100

    Add new categories for Video Editing and Crypto

--- store/migrations/0003_update_categories.py
+++ store/migrations/0003_update_categories.py
@@ -19,6 +19,8 @@ def update_categories(apps, schema_editor):
         ('Textiles', 'textiles'),
         ('Shedda', 'shedda'),
         ('Gatzner', 'gatzner'),
+        ('Video Editing', 'video editing'),
+        ('Crypto', 'crypto'),
     ]
     
     for name, slug in new_categories:
