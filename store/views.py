80e12946bf40611729025eb027cae289c152f647
Mohammed Mohammed Ibrahim <72116613+mohammedimk@users.noreply.github.com>
2026-07-11 14:50:13 +0100

    add csrf exempt

--- store/views.py
+++ store/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.db.models import F, Q
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.csrf import csrf_exempt
 
 from .forms import AdminLoginForm, AdminRegisterForm, ProductForm, SiteSettingForm
 from .models import Category, Product, SiteSetting
@@ -133,6 +134,7 @@ def search_view(request):
 
 
 @require_http_methods(["POST"])
+@csrf_exempt
 def checkout(request):
     """
     >>> CHECKOUT PAGE <<<
