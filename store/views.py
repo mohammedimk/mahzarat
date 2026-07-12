8c9c6ccf4d24171434c11f15c4d4659cd3127aa7
Mohammed Mohammed Ibrahim <72116613+mohammedimk@users.noreply.github.com>
2026-07-12 15:34:35 +0100

    Update views.py

--- store/views.py
+++ store/views.py
@@ -490,11 +490,6 @@ def delete_bank_account(request):
     return redirect('store:admin_dashboard')
     
 
-from django.shortcuts import render, redirect, get_object_or_4 Python
-from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
-from django.contrib import messages
-from .models import Order
 
 @login_required(login_url='store:admin_login')
 def admin_orders(request):
